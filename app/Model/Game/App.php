cfa41bc6036f3b949ea3291c07a2e47dd2d193f4
agdholo <agdholo@gmail.com>
2018-08-28 22:03:49 +0800

    add: app model relationship

--- app/Model/Game/App.php
+++ app/Model/Game/App.php
@@ -8,4 +8,19 @@ class App extends Model
 {
     protected $table = 'Apps';
     public $timestamps = false;
+
+    public function AppType()
+    {
+        return $this->hasOne('App\Model\Game\AppType', 'AppType', 'AppType');
+    }
+
+    public function AppPrice()
+    {
+        return $this->hasMany('App\Model\Game\AppPrice', 'AppID', 'AppID');
+    }
+
+    public function AppInfo()
+    {
+        return $this->hasMany('App\Model\Game\AppInFo', 'AppID', 'AppID');
+    }
 }
