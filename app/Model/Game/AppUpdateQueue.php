3cb8de79e5163e3a5bac311406b02264bc01911f
agdholo <agdholo@gmail.com>
2018-08-28 22:47:12 +0800

    create: store update queue model

---
+++ app/Model/Game/AppUpdateQueue.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Model\Game;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AppUpdateQueue extends Model
+{
+    protected $table = 'StoreUpdateQueue';
+    public $timestamps = false;
+}
