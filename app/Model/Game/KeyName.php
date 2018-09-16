28bb53fab8aa358436dadf619bb9fc16d4e88c87
agdholo <agdholo@gmail.com>
2018-09-16 12:26:06 +0800

    add: app info function

---
+++ app/Model/Game/KeyName.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Model\Game;
+
+use Illuminate\Database\Eloquent\Model;
+
+class KeyName extends Model
+{
+    protected $table = 'KeyNames';
+    public $timestamps = false;
+}
