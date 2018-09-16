21f7a74a85211b615c70f15645333c65a0d98bd1
agdholo <agdholo@gmail.com>
2018-09-16 12:25:28 +0800

    add: app details curl

--- app/Http/Controllers/V2/App/DetailController.php
+++ app/Http/Controllers/V2/App/DetailController.php
@@ -2,10 +2,20 @@
 
 namespace App\Http\Controllers\V2\App;
 
-use Illuminate\Http\Request;
+use Cache;
+use GuzzleHttp\Client;
 use App\Http\Controllers\Controller;
 
 class DetailController extends Controller
 {
-    //
+    public function show($id) {
+        return Cache::remember('appdetails=' . $id, 360, function () use ($id) {
+            $client = new Client([
+                'base_uri' => 'http://localhost',
+                'timeout'  => 10.0,
+            ]);
+            $response = $client->request('GET', 'https://store.steampowered.com/api/appdetails?appids=' . $id . '&l=schinese');
+            return json_decode((string) $response->getBody(), true);
+        });
+    }
 }
