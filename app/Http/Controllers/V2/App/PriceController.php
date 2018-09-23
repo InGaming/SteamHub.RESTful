45289bd8c6cc609021edde2486a079a9fd260029
agdholo <agdholo@gmail.com>
2018-09-23 12:23:31 +0800

    add: country switch in price of apps

--- app/Http/Controllers/V2/App/PriceController.php
+++ app/Http/Controllers/V2/App/PriceController.php
@@ -11,13 +11,23 @@ class PriceController extends Controller
 {
     public function show($id) {
         $count = Request::get('count');
+        $cc = Request::get('cc');
+        if ($cc === 'cn') $country = 'cn';
+        else if ($cc === 'us') $country = 'us';
+        else $country = 'cn';
         if ($count == 'min') {
-            return Cache::remember(Request::fullUrl(), 360, function () use ($id) {
-                return AppPrice::where('AppID', $id)->where('Country', 'China')->min('PriceFinal');
+            return Cache::remember(Request::fullUrl(), 360, function () use ($id, $country) {
+                if ($country === 'cn')
+                    return AppPrice::where('AppID', $id)->where('Country', 'China')->min('PriceFinal');
+                if ($country === 'us')
+                return AppPrice::where('AppID', $id)->where('Country', 'United States')->min('PriceFinal');
             });
         }
-        return Cache::remember(Request::fullUrl(), 360, function () use ($id) {
-            return AppPrice::where('AppID', $id)->where('Country', 'China')->orderBy('LastUpdated', 'desc')->get();
+        return Cache::remember(Request::fullUrl(), 360, function () use ($id, $country) {
+            if ($country === 'cn')
+                return AppPrice::where('AppID', $id)->where('Country', 'China')->orderBy('LastUpdated', 'desc')->get();
+            if ($country === 'us')
+            return AppPrice::where('AppID', $id)->where('Country', 'United States')->orderBy('LastUpdated', 'desc')->get();
         });
     }
 }
