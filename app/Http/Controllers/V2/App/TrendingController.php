567702ea14ccf121b1f180d5c1b52aa6e4c9b463
AGD <agdholo@gmail.com>
2018-11-07 14:56:02 +0800

    add: game trending total list for today data method

--- app/Http/Controllers/V2/App/TrendingController.php
+++ app/Http/Controllers/V2/App/TrendingController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\V2\App;
 
 use Cache;
+use Carbon;
 use Request;
 use App\Http\Controllers\Controller;
 use App\Model\Game\Trending;
@@ -12,8 +13,12 @@ class TrendingController extends Controller
     public function index ()
     {
         return Cache::remember(Request::fullUrl(), 5, function () {
-            $data = Trending::latest('Created')->orderBy('Now', 'desc')->limit(100)->get();
-            return $data;
+            return Trending::latest('Created')->orderBy('Now', 'desc')->limit(100)->get();
+
+            // 
+            if (Request::get('type') == 'total' && Request::get('count') == 'max' && Request::get('date') == 'today') {
+                return Trending::whereDate('Created', Carbon::today())->orderBy('Now', 'desc')->unique('AppID')->all();
+            }
         });
     }
 }
