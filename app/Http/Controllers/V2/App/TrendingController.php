2d9b6192b802fad7b00b4d8bbfe4dacbf2b1bd7c
AGD <agdholo@gmail.com>
2018-11-07 15:26:32 +0800

    fix: Incorrect collection statement

--- app/Http/Controllers/V2/App/TrendingController.php
+++ app/Http/Controllers/V2/App/TrendingController.php
@@ -15,7 +15,7 @@ class TrendingController extends Controller
         return Cache::remember(Request::fullUrl(), 5, function () {
             // 
             if (Request::get('type') == 'total' && Request::get('count') == 'max' && Request::get('date') == 'today') {
-                return Trending::whereDate('Created', Carbon::today())->orderBy('Total', 'asc')->unique('AppID')->values()->all();
+                return Trending::whereDate('Created', Carbon::today())->orderBy('Total', 'desc')->get()->unique('AppID')->values()->all();
             } else {
                 return Trending::latest('Created')->orderBy('Now', 'desc')->limit(100)->get();
             }
