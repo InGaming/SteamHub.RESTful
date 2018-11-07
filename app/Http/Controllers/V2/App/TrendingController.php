b034e82659611c35d6514a75568d96d3ea0f47e4
AGD <agdholo@gmail.com>
2018-11-07 15:06:07 +0800

    fix: Trending Judging logical order

--- app/Http/Controllers/V2/App/TrendingController.php
+++ app/Http/Controllers/V2/App/TrendingController.php
@@ -13,11 +13,11 @@ class TrendingController extends Controller
     public function index ()
     {
         return Cache::remember(Request::fullUrl(), 5, function () {
-            return Trending::latest('Created')->orderBy('Now', 'desc')->limit(100)->get();
-
             // 
             if (Request::get('type') == 'total' && Request::get('count') == 'max' && Request::get('date') == 'today') {
                 return Trending::whereDate('Created', Carbon::today())->orderBy('Total', 'asc')->unique('AppID')->all();
+            } else {
+                return Trending::latest('Created')->orderBy('Now', 'desc')->limit(100)->get();
             }
         });
     }
