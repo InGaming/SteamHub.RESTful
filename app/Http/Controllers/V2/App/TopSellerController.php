8fb2238beaa1643af3160ba65cefa859c54310a8
AGD <agdholo@gmail.com>
2018-11-09 12:11:46 +0800

    fix: topseller data fetch

--- app/Http/Controllers/V2/App/TopSellerController.php
+++ app/Http/Controllers/V2/App/TopSellerController.php
@@ -15,8 +15,7 @@ class TopSellerController extends Controller
         if ($cc == 'cn') { $country = 'China'; }
         if ($cc == 'us') { $country = 'United States'; }
         return Cache::remember(Request::fullUrl(), 60, function () use ($country) {
-            $firstData = TopSeller::where('Country', $country)->latest('LastUpdated')->first();
-            return response()->json($firstData->Data);
+            return TopSeller::latest('Created')->limit(100)->get();
         });
     }
 }
