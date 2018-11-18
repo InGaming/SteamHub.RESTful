0947ecf0a988eca9f66a9e413baf57633aab41e5
AGD <agdholo@gmail.com>
2018-11-18 12:02:47 +0800

    Optimization: Elegant implementation of search queries

--- app/Http/Controllers/Game/SearchController.php
+++ app/Http/Controllers/Game/SearchController.php
@@ -13,7 +13,7 @@ use App\Model\Game\AppTag;
 class SearchController extends Controller
 {
     public function index(Request $request) {
-        return Cache::remember($request->fullUrl(), 1440, function () use ($request) {
+        return Cache::remember($request->fullUrl(), 0, function () use ($request) {
             $type = $request->type;
             $price = $request->price;
             $query_name = $request->q[0];
@@ -22,67 +22,43 @@ class SearchController extends Controller
     }
 
     public function query($query_name, $price, $type) {
-
-        $app = App::query();
         
-        if ($query_name) {
-            $appid = $app->where('AppID', 'like', '%' . $query_name . '%')
-            ->orWhere('Name', 'like', '%' . $query_name . '%')
-            ->orWhere('ChineseName', 'like', '%' . $query_name . '%')
-            ->orWhere('Nickname', 'like', '%' . $query_name . '%')
-            ->orWhere('DetailedDescription', 'like', '%' . $query_name . '%')
-            ->orWhere('ShortDescription', 'like', '%' . $query_name . '%')
-            ->whereNotIn('AppType', [0])
-            ->distinct()
-            ->pluck('AppID');
-        } else {
-            $appid = false;
-        }
-
         if ($price[0]) {
             $unique_price = collect($price)->unique()->values()->all();
         } else {
             $unique_price = ['0, 999900'];
         }
 
-
-        if ($appid) {
-            $appPrice = AppPrice::whereIn('AppID', $appid);
-        } else {
-            $appPrice = AppPrice::query();
-        }
-        
+        $appPrice = AppPrice::query();
         $appTag = AppTag::query();
         
+        $app = App::where('AppID', 'like', '%' . $query_name . '%')
+        ->orWhere('Name', 'like', '%' . $query_name . '%')
+        ->orWhere('ChineseName', 'like', '%' . $query_name . '%')
+        ->orWhere('Nickname', 'like', '%' . $query_name . '%')
+        ->orWhere('DetailedDescription', 'like', '%' . $query_name . '%')
+        ->orWhere('ShortDescription', 'like', '%' . $query_name . '%')
+        ->whereNotIn('AppType', [0]);
+        
         foreach ($unique_price as $key=>$field) {
-            $appPriceId = $appPrice
-            ->where('Country', 'China')
-            ->whereNotNull('PriceInitial')
-            ->whereBetween('PriceInitial', explode(',', $field))
-            ->distinct()
-            ->pluck('AppID');
-
-            $appTagId = $appTag
-            ->when($type[0], function ($query) use ($type) {
-                $query->whereIn('Tag', $type);
+            $data[$key] = $app->whereHas('AppPrice', function ($query) use ($field) {
+                return $query->whereBetween('PriceInitial', explode(',', $field))->where('Country', 'China')->whereNotNull('PriceInitial');
             })
-            ->whereIn('AppID', $appPriceId)
-            ->distinct()
-            ->pluck('AppID');
-
-            $data[$key] = $app
-            ->whereIn('AppID', $appTagId)
             ->with([
                 'AppPrice' => function ($query) {
-                    $query->where('Country', 'China')->whereNotNull('PriceInitial')->orderBy('LastUpdated', 'desc');
-                }, 
-                'AppTag' => function ($query) {
-                    $query->orderBy('LastUpdated', 'desc');
-                }, 
-                'AppReview' => function ($query) {
-                    $query->orderBy('LastUpdated', 'desc');
-                },
+                    return $query->where('Country', 'China')->orderBy('LastUpdated', 'desc');
+                }
             ])
+            ->when($type[0], function ($query) use ($type) {
+                $query->whereHas('AppTag', function ($query) use ($type) {
+                    return $query->whereIn('Tag', $type);
+                })
+                ->with([
+                    'AppTag' => function ($query) {
+                        return $query->orderBy('LastUpdated', 'desc');
+                    }
+                ]);
+            })
             ->orderBy('LastUpdated', 'desc')
             ->paginate();
         }
