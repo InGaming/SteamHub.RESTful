844db82226bfeb749fdf9a5eed5016adfb475f8d
AGD <agdholo@gmail.com>
2018-11-26 11:25:57 +0800

    chore: add "run in postman" in docs view.

--- resources/views/docs/welcome.blade.php
+++ resources/views/docs/welcome.blade.php
@@ -63,9 +63,19 @@
                 <div class="title m-b-md">
                     {{ config('app.name') }} Documentation
                 </div>
-
+                <div class="postman-run-button"
+                     data-postman-action="collection/import"
+                     data-postman-var-1="de562630d6c6b9da94b8"></div>
+                <script type="text/javascript">
+                    (function (p,o,s,t,m,a,n) {
+                        !p[s] && (p[s] = function () { (p[t] || (p[t] = [])).push(arguments); });
+                        !o.getElementById(s+t) && o.getElementsByTagName("head")[0].appendChild((
+                            (n = o.createElement("script")),
+                                (n.id = s+t), (n.async = 1), (n.src = m), n
+                        ));
+                    }(window, document, "_pm", "PostmanRunObject", "https://run.pstmn.io/button.js"));
+                </script>
                 <div class="links">
-                    <a href="/api/{{ config('app.version') }}">API</a>
                     <a href="{{ config('app.website_url') }}">Website</a>
                     <a href="{{ config('app.github_url') }}">GitHub</a>
                 </div>
